b71ba4c4328160f01f4a971c3b1be6070d5af6f5
Srulik P <srulikpinson770@gmail.com>
2024-10-20 13:05:13 -0600

    proper temporary camera bounds

--- engine/Actors/Background.js
+++ engine/Actors/Background.js
@@ -1,11 +1,12 @@
 class Background extends ex.Actor {
     constructor(height, width) {
         super({
-            x: width / 3,
-            y: height / 4,
+            x: 0,
+            y: 0,
             width: width,
             height: height,
         });
+        this.anchor = new ex.vec(0, 0);
     };
     onInitialize() {
         this.boundingBox = new ex.BoundingBox(this.x, this.y, this.x + this.width, this.y + this.height)
--- engine/beh.js
+++ engine/beh.js
@@ -1,13 +1,11 @@
 //add background
 const background1 = new Background(3200, 4400);
 
-
-
 //add player
 const you = new Player(622, 568, ex.Color.Green, 2.5);
 
 //ad npcs
-const mendel = new NPC(553, 313);
+const mendel = new NPC(553, 1000);
 const ita = new NPC(800, 1200, ex.Color.Black);
 const chanale = new NPC(800, 800, ex.Color.Blue);
 const mushka = new NPC(189, 249, ex.Color.Yellow);
